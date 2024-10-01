de2c7c0cfdfc0bf3df93c16d4d78d96390749fc5
Hoerudin <hoerudinpermana30@gmail.com>
2024-10-01 16:40:05 +0700

    if else

--- if_else.js
+++ if_else.js
@@ -7,3 +7,20 @@ if (gajian) {
 
 console.log("Dirumah aja");
 
+const score = 80;
+
+if (score >= 80) {
+  console.log("selamat anda lulus");
+} else {
+  console.log("mohon maaf anda belum lulus");
+}
+
+const nilai = 95;
+
+if (nilai > 90) {
+  console.log("Selamat Anda lulus dengan nilai A");
+} else if (nilai > 85) {
+  console.log("Selamat Anda lulus");
+} else {
+  console.log("Mohon maaf Anda belum lulus");
+}
